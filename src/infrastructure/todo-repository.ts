0d6001e8ca1c6698e3dc6eff33d13c60bf771943
Yeonu-Kim <ywk0524@snu.ac.kr>
2026-03-10 20:03:05 +0900

    ✨ 이전 주차 코드 불러오기

---
+++ src/infrastructure/todo-repository.ts
@@ -0,0 +1,49 @@
+import fs from 'fs';
+import path from 'path';
+import { TODO_FILE_PATH } from '../domain/file-root';
+import type { Todo, TodoRepository } from '../domain/todo-interface';
+
+const TODO_FILE = path.resolve(process.cwd(), TODO_FILE_PATH);
+
+const todoTypeGuard = (value: unknown): value is Todo => {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    'id' in value &&
+    'content' in value &&
+    'done' in value &&
+    typeof (value as Todo).id === 'number' &&
+    typeof (value as Todo).content === 'string' &&
+    typeof (value as Todo).done === 'boolean'
+  );
+};
+
+export const implTodoRepository = (): TodoRepository => {
+  if (!fs.existsSync(TODO_FILE)) {
+    fs.writeFileSync(TODO_FILE, '[]', 'utf-8');
+  }
+
+  return {
+    readTodos: () => {
+      try {
+        const raw = fs.readFileSync(TODO_FILE, 'utf-8');
+        const parsed: unknown = JSON.parse(raw);
+
+        if (!Array.isArray(parsed) || !parsed.every(todoTypeGuard)) {
+          return { state: 'error', detailedError: 'INVALID_JSON_FORMAT' };
+        }
+        return { state: 'success', data: parsed };
+      } catch {
+        return { state: 'error', detailedError: 'FILE_READ_FAILED' };
+      }
+    },
+    writeTodos: ({ todos }) => {
+      try {
+        fs.writeFileSync(TODO_FILE, JSON.stringify(todos, null, 2), 'utf-8');
+        return { state: 'success' };
+      } catch {
+        return { state: 'error', detailedError: 'FILE_WRITE_FAILED' };
+      }
+    },
+  };
+};
